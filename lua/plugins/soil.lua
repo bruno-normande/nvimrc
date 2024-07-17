6a5c9064287fbe2033d2a555313e97117b2a95de
Bruno Normande <linsbrun@amazon.com>
2024-07-16 17:34:55 -0700

    fix: soil

--- lua/plugins/soil.lua
+++ lua/plugins/soil.lua
@@ -1,23 +1,23 @@
 return {
-	-- plantuml para poder fazer os diagramas de classe e tal
-	"javiorfo/nvim-soil",
-	lazy = true,
-	ft = "plantuml",
-	config = function()
-		require("nvim-soil").setup({
-			image = {
-				darkmode = false, -- Enable or disable darkmode
-				format = "png", -- Choose between png or svg
+    -- plantuml para poder fazer os diagramas de classe e tal
+    "javiorfo/nvim-soil",
+    dependencies = { "javiorfo/nvim-nyctophilia" },
 
-				-- This is a default implementation of using nsxiv to open the resultant image
-				-- Edit the string to use your preferred app to open the image
-				-- Some examples:
-				-- return "feh " .. img
-				-- return "xdg-open " .. img
-				execute_to_open = function(img)
-					return "open  " .. img
-				end,
-			},
-		})
-	end,
+    lazy = true,
+    ft = "plantuml",
+    opts = {
+        image = {
+            darkmode = false, -- Enable or disable darkmode
+            format = "png", -- Choose between png or svg
+
+            -- This is a default implementation of using nsxiv to open the resultant image
+            -- Edit the string to use your preferred app to open the image
+            -- Some examples:
+            -- return "feh " .. img
+            -- return "xdg-open " .. img
+            execute_to_open = function(img)
+                return "open  " .. img
+            end,
+        },
+    }
 }
